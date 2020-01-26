885ef2ac2874696783d93e41f97870cb5468d3c9
chingsubam <kchings1@binghamton.edu>
2020-01-25 22:51:19 -0500

    a

---
+++ arr.js
@@ -0,0 +1,11 @@
+var posts=[
+{
+	title: "Cats are mediocre",
+	author: "Colt",
+	comments: ["Awesome post", "terrible post"]
+},
+{
+	title:"Cats are actually awesome",
+	author: "Cat lover",
+	comments: ["bla", "go to hell"]
+}]
